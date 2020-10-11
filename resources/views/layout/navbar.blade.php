4a50e345f0c4c80c71fb19e9290c03a438683eef
OmarXtream <o20171900@gmail.com>
2020-10-11 13:08:30 +0300

    Register Nav

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -16,6 +16,10 @@
           <li class="nav-item">
             <a class="nav-link" href="{{route('TeacherLogin')}}">بوابة المعلم</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('register')}}">تسجيل جديد</a>
+          </li>
+
         </ul>
       </div>
     </div>
